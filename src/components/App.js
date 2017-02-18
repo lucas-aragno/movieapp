4edb5f813803d4a72b12fac77a114194805ae785
lucas <lucas.aragno157@gmail.com>
2017-02-18 18:17:04 -0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,21 @@
 import { h, Component } from 'preact'
 
 export default class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      searches: []
+    }
+  }
+
+  addSearch ({search, results}) {
+    this.setState((prevState) => {
+      const { searches } = prevState
+      searches.push({search, results})
+      return {searches: searches}
+    })
+  }
+
   render () {
     return (
       <div>
